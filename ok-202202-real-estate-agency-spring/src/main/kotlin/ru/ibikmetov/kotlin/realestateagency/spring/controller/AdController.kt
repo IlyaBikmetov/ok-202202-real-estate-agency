c2d7995d866695caf0551d5a022e70a539bd7511
Ilya Bikmetov <ilyab_v@mail.ru>
2022-10-01 15:36:29 +0500

    Home work Logs1

--- ok-202202-real-estate-agency-spring/src/main/kotlin/ru/ibikmetov/kotlin/realestateagency/spring/controller/AdController.kt
+++ ok-202202-real-estate-agency-spring/src/main/kotlin/ru/ibikmetov/kotlin/realestateagency/spring/controller/AdController.kt
@@ -3,39 +3,41 @@ package ru.ibikmetov.kotlin.realestateagency.spring.controller
 import org.springframework.web.bind.annotation.*
 import ru.ibikmetov.kotlin.realestateagency.api.v1.models.*
 import ru.ibikmetov.kotlin.realestateagency.common.models.ReAgCommand
+import ru.ibikmetov.kotlin.realestateagency.logging.mpLogger
 import ru.ibikmetov.kotlin.realestateagency.spring.service.AdService
 
 @RequestMapping("ad")
 @RestController
 class AdController(private val adService: AdService) {
+    private val logger = mpLogger(this::class.java)
 
     @PostMapping("create")
     fun adCreate(@RequestBody request: AdCreateRequest): AdCreateResponse =
-        controllerHelper(request, ReAgCommand.CREATE) {
+        controllerHelper(request, logger, "ad-create", ReAgCommand.CREATE) {
             adService.createAd(this)
         }
 
     @PostMapping("read")
     fun adRead(@RequestBody request: AdReadRequest): AdReadResponse =
-        controllerHelper(request, ReAgCommand.READ) {
+        controllerHelper(request, logger, "ad-read", ReAgCommand.READ) {
             adService.readAd(this)
         }
 
     @RequestMapping("update", method = [RequestMethod.POST])
     fun adUpdate(@RequestBody request: AdUpdateRequest): AdUpdateResponse =
-        controllerHelper(request, ReAgCommand.UPDATE) {
+        controllerHelper(request, logger, "ad-update", ReAgCommand.UPDATE) {
             adService.updateAd(this)
         }
 
     @PostMapping("delete")
     fun adDelete(@RequestBody request: AdDeleteRequest): AdDeleteResponse =
-        controllerHelper(request, ReAgCommand.DELETE) {
+        controllerHelper(request, logger, "ad-delete", ReAgCommand.DELETE) {
             adService.deleteAd(this)
         }
 
     @PostMapping("search")
     fun adSearch(@RequestBody request: AdSearchRequest): AdSearchResponse =
-        controllerHelper(request, ReAgCommand.SEARCH) {
+        controllerHelper(request, logger, "ad-search", ReAgCommand.SEARCH) {
             adService.searchAd(this)
         }
 }
